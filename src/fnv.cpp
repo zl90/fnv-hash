da5314b8e035f92fc718a599dfc93b9d7bb0228e
zl90 <101400977+zl90@users.noreply.github.com>
2023-09-17 21:49:42 +1000

    Add implementation

---
+++ src/fnv.cpp
@@ -0,0 +1,36 @@
+// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+
+#include <string>
+#include <iostream>
+#include "test_utils.h"
+
+using namespace std;
+
+#define FNV_OFFSET_BASIS 14695981039346656037
+#define FNV_PRIME 1099511628211
+
+unsigned long long fnvHash(string data)
+{
+    unsigned long long hash = FNV_OFFSET_BASIS;
+
+    for (int i : data)
+    {
+        hash = hash * FNV_PRIME;
+        hash = hash ^ i;
+    }
+
+    return hash;
+}
+
+void runTests()
+{
+    expect("test1", fnvHash("hello"), 8883723591023973575);
+    expect("test2", fnvHash("world"), 4521841679625769999);
+}
+
+int main()
+{
+    runTests();
+
+    return 0;
+}
